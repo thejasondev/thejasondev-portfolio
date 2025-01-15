f714579adb6c4ab3dbd8b139c6b7965793e3b250
thejasondev <jason.guerra1002@gmail.com>
2025-01-15 14:31:29 -0500

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/thejasondev-portfolio/" : "/",
   integrations: [tailwind()],
