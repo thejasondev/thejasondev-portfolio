352e8e5d1d6c3f9dad3c975fac06ab93930da2f6
thejasondev <jason.guerra1002@gmail.com>
2025-01-17 16:18:08 -0500

    bog changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/thejasondev-portfolio/" : "/",
+  site: 'https://thejasondev.github.io',
+  base: '/thejasondev-portfolio',
   build: {
-    assets: "_astro",
-    assetsPrefix: process.env.NODE_ENV === "production" ? "." : ""
+    assets: '_astro'
   },
   integrations: [tailwind()],
 });
