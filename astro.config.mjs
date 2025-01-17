e2f2e689e52425b20ea660a2ea7de4a24f349047
thejasondev <jason.guerra1002@gmail.com>
2025-01-17 17:13:27 -0500

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://thejasondev.github.io',
   base: '/thejasondev-portfolio',
+  outDir: './dist',
   build: {
-    assets: '_assets'
+    assets: 'assets'
   },
   integrations: [tailwind()],
   vite: {
     build: {
-      assetsInlineLimit: 0
+      assetsInlineLimit: 0,
+      cssCodeSplit: false
     }
   }
 });
